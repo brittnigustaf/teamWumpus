c8469dac15fd49ebb861057246b4b3416e8ad8c4
jessdev <jessdev@umich.edu>
2013-11-17 11:52:32 -0500

    updates Trap.class and Wumpus.class

--- eecs285.proj4.wumpus/src/eecs285/proj4/wumpus/Trap.java
+++ eecs285.proj4.wumpus/src/eecs285/proj4/wumpus/Trap.java
@@ -1,15 +1,32 @@
 package eecs285.proj4.wumpus;
 
-/** Purpse: abstract class for trap.
- * Alows people to impliment many different types of traps like
+/** Purpose: abstract class for trap.
+ * Allows people to impliment many different types of traps like
  * wumpuses that move. Or pits. class must implement call hint
  * 
  * @author Jessica DeVirese
  */
 
 abstract class Trap {
+  //member variables
+  Room location;
   
   abstract String callHint();
   //EFF: returns a string
+  
+  Trap(Room inRoom){
+    //EFF: traps must be assigned a room, and thus a room gets a trap
+    //MOD: location
+    
+    location = inRoom;
+    location.add(this);
+  }
+  
+  void add(Room inRoom, String direction){
+    //EFF: changes the location for the trap
+    //MOD: location
+    
+    location = inRoom;
+  }
 
 }
