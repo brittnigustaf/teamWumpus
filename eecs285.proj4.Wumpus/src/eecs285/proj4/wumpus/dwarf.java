e494a8ab720f53d7a7088b050445bd8759fc2962
jessdev <jessdev@umich.edu>
2013-11-26 21:32:33 -0500

    It is a dwarf! It does nothing right now

---
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/dwarf.java
@@ -0,0 +1,13 @@
+package eecs285.proj4.wumpus;
+
+/** Purpose: dwarf constructs the map given a bluePrint.
+ * It requires a place to build: tile map
+ * It requires a blueprint: filename
+ * 
+ * @author Jessica DeVriese
+ */
+
+public class dwarf {
+  
+
+}
