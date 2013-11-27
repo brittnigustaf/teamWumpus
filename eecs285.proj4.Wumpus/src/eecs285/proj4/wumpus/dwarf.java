752b5ae247dc2f951f9e7d23fbc2c0f9b890c0a9
jessdev <jessdev@umich.edu>
2013-11-26 22:58:44 -0500

    might read files

--- eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/dwarf.java
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/dwarf.java
@@ -1,13 +1,39 @@
 package eecs285.proj4.wumpus;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 /** Purpose: dwarf constructs the map given a bluePrint.
- * It requires a place to build: tile map
  * It requires a blueprint: filename
  * 
  * @author Jessica DeVriese
  */
 
 public class dwarf {
+  //Member variables
+  Room dungeon[][];
+  FileInputStream fis;
+  InputStreamReader in;
   
+
+  
+  dwarf(String Filename, Room inDungeon[][]){
+    //
+    
+    try {
+      fis = new FileInputStream(Filename);
+    } catch (FileNotFoundException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    try {
+      in = new InputStreamReader(fis, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
 }
