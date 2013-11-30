287ae983b7d6ef815536f8a6ec5ac62a8d6282b1
lithas@umich.edu <lithas@umiche.du>
2013-11-29 19:54:51 -0500

    getting there on map-making. Just need to cement the correct attributes for each tile into place

--- eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/MapMaker.java
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/MapMaker.java
@@ -1,9 +1,12 @@
 package eecs285.proj4.wumpus;
 
 import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class MapMaker {
     
@@ -21,28 +24,43 @@ public class MapMaker {
         T_DOWN,
         EMPTY;
     }
+    public enum Direction{
+    	UP,
+    	RIGHT,
+    	DOWN,
+    	LEFT;
+    }
     
     private class littleRoom{
-        roomTypes type;
+        roomTypes type = roomTypes.EMPTY;
+        Set <Direction> canGo = new HashSet<Direction>();
+        boolean isPit = false;
+        boolean isBat = false;
+        boolean P1Start = false;
+        boolean P2Start = false;
+        boolean wumpusStart = false;
     }
     
     public static int main(String [] args){
         Scanner scanner = new Scanner (System.in);
-        System.out.println("Please input the dimensions of the map in the form: X Y");
+        System.out.println(
+        		"Please input the dimensions of the map in the form: X Y");
         String input = scanner.next(); // Get what the user types.
         
         
         input = input.trim();
         String dimensions[] = input.split(" ");
         
-        System.out.println("Please input the number of pit-traps you'd like:");
+        System.out.println(
+        		"Please input the number of pit-traps you'd like:");
         input = scanner.next(); // Get what the user types.
         
         input = input.trim();
         
         int numPits = Integer.parseInt(input);
         
-        System.out.println("Please input the number of bat-traps you'd like:");
+        System.out.println(
+        		"Please input the number of bat-traps you'd like:");
         input = scanner.next(); // Get what the user types.
         
         input = input.trim();
@@ -77,7 +95,10 @@ public class MapMaker {
         int start1x = start1 / x;
         int start1y = start1 % x;
         
-        map[start1x][start1y] += "1"; // set that square to be the starting square for P1
+        map[start1x][start1y] += "1"; // set that square to be the 
+        							  //starting square for P1
+        rooms[start1x][start1y].P1Start = true;
+
         
         int start2;
         do{ //make sure start2 and start1 aren't the same square;
@@ -88,8 +109,10 @@ public class MapMaker {
         int start2x = start2 / x;
         int start2y = start2 % x;
         
-        map[start2x][start2y] += "2"; // set that square to be the starting square for P2
-
+        map[start2x][start2y] += "2"; // set that square to be the 
+         							  //starting square for P2
+        rooms[start2x][start2y].P2Start = true;
+     
         int wumpusSquare;
         do{ //make sure start2 and start1 aren't the same square;
             wumpusSquare = rando.nextInt() % size;
@@ -99,7 +122,46 @@ public class MapMaker {
         int wumpusx = wumpusSquare / x;
         int wumpusy = wumpusSquare % x;
         
-        map[start2x][start2y] += "W"; // set that square to be the starting square for the wumpus
+        map[wumpusx][wumpusy] += "W"; // set that square to be the 
+        							  //starting square for the wumpus
+        rooms[wumpusx][wumpusy].wumpusStart = true;
+        
+        Integer batSquares[] = new Integer[numBats];
+        int numFound = 0;
+        for(int i = 0; i < numBats; i++){
+	        do{ //make sure start2 and start1 aren't the same square;
+	            batSquares[numFound] = rando.nextInt() % size;
+	        }
+	        while(batSquares[numFound] == start1 || 
+	        		batSquares[numFound] == start2);
+        
+	        int batx = batSquares[numFound] / x;
+	        int baty = batSquares[numFound] % x;
+	        
+	        numFound++;
+	        
+	        map[batx][baty] += "b"; // set that square to have bats
+	        rooms[batx][baty].isBat = true;
+        }
+        
+        Integer pitSquares[] = new Integer[numPits];
+        numFound = 0;
+        for(int i = 0; i < numPits; i++){
+	        do{ //make sure start2 and start1 aren't the same square;
+	            pitSquares[numFound] = rando.nextInt() % size;
+	        }
+	        while(pitSquares[numFound] == start1 ||
+	        		pitSquares[numFound] == start2);
+        
+	        int pitx = pitSquares[numFound] / x;
+	        int pity = pitSquares[numFound] % x;
+	        
+	        numFound++;
+	        
+	        map[pitx][pity] += "b"; // set that square to have a pit
+	        rooms[pitx][pity].isPit = true;
+        }
+        
         
         while(!isPath(map, rooms)){
             generateMap(map,rooms);
@@ -112,16 +174,128 @@ public class MapMaker {
     }
 
     private static void printMap(String[][] map, littleRoom[][] rooms) {
-        // TODO Auto-generated method stub
-        
+    	//prints out a map of wumpus rooms.
+		System.out.println("----------------------------------------------------");
+    	for(String[] row: map){
+    		String line = "|";
+    		for(String room: row){
+    			int bufLen = 5;
+    			bufLen-=room.length();
+    			line += room;
+    			for(int i = 0; i < bufLen; i++){
+    				line += " ";
+    			}
+    			line += "|";
+    			
+    		}
+    		System.out.println(line);
+    		System.out.println("----------------------------------------------------");
+    		
+    	}
+    
+    	return;
     }
 
     private static void generateMap(String[][] map, littleRoom[][] rooms) {
-        // TODO Auto-generated method stub
-        
+    	//generates a random map for hunt the wumpus 
+    	//given the dimensions of the map.
+    	int x = map.length;
+    	int y = map[0].length;
+    	Set<roomTypes> options = new HashSet<roomTypes>();
+    	for (roomTypes tmp: roomTypes.values()){
+    		options.add(tmp);
+    	}
+    	Set<roomTypes> up = new HashSet<roomTypes>();
+    	up.add(roomTypes.CROSS);
+    	up.add(roomTypes.UP_RIGHT);
+    	up.add(roomTypes.UP_LEFT);
+    	up.add(roomTypes.UP_DOWN);
+    	up.add(roomTypes.T_UP);
+    	up.add(roomTypes.T_LEFT);
+    	up.add(roomTypes.T_RIGHT);
+    	
+    	Set<roomTypes> down = new HashSet<roomTypes>();
+    	down.add(roomTypes.CROSS);
+    	down.add(roomTypes.DOWN_RIGHT);
+    	down.add(roomTypes.DOWN_LEFT);
+    	down.add(roomTypes.UP_DOWN);
+    	down.add(roomTypes.T_DOWN);
+    	down.add(roomTypes.T_LEFT);
+    	down.add(roomTypes.T_RIGHT);
+    	
+    	Set<roomTypes> left = new HashSet<roomTypes>();
+    	left.add(roomTypes.CROSS);
+    	left.add(roomTypes.LEFT_RIGHT);
+    	left.add(roomTypes.UP_LEFT);
+    	left.add(roomTypes.DOWN_LEFT);
+    	left.add(roomTypes.T_UP);
+    	left.add(roomTypes.T_LEFT);
+    	left.add(roomTypes.T_DOWN);
+    	
+    	Set<roomTypes> right = new HashSet<roomTypes>();
+    	right.add(roomTypes.CROSS);
+    	right.add(roomTypes.UP_RIGHT);
+    	right.add(roomTypes.DOWN_RIGHT);
+    	right.add(roomTypes.LEFT_RIGHT);
+    	right.add(roomTypes.T_UP);
+    	right.add(roomTypes.T_DOWN);
+    	right.add(roomTypes.T_RIGHT);
+    	
+    	for(int i = 0; i < x; i ++){
+    		for(int j = 0; j < y; j++){
+    			Set<roomTypes> roomOptions = new HashSet<roomTypes>();
+    			roomOptions.addAll(options);
+    			
+    			//first, if it's an edge, don't let them go over the edge
+    			if(i == 0){ //can't go up
+    				roomOptions.removeAll(up);	
+    			}
+    			if(j == 0){//can't go left
+    				roomOptions.removeAll(left);
+    			}
+    			if(i == x - 1){//can't go down
+    				roomOptions.removeAll(down);
+    			}
+    			if(j == y - 1){//can't go right
+    				roomOptions.removeAll(right);
+    			}
+    			//now check neighbors for things I have to follow
+    			Set<Direction> requires = new HashSet<Direction>();
+    			
+    			//get the intersection of options and requirements
+    			if(j+1 < y && rooms[i][j+1].type != roomTypes.EMPTY){
+    				if(rooms[i][j+1].canGo.contains(Direction.LEFT) ){
+    					requires.add(Direction.RIGHT);
+    					roomOptions.retainAll(right);
+    				}
+    			}
+    			if(i+1 < x && rooms[i+1][j].type != roomTypes.EMPTY){
+    				if(rooms[i+1][j].canGo.contains(Direction.UP) ){
+    					requires.add(Direction.DOWN);
+    					roomOptions.retainAll(down);
+    				}
+    			}
+    			if(j-1 >= 0 && rooms[i][j-1].type != roomTypes.EMPTY){
+    				if(rooms[i][j-1].canGo.contains(Direction.RIGHT) ){
+    					requires.add(Direction.LEFT);
+    					roomOptions.retainAll(left);
+    				}
+    			}
+    			if(i -1 >= 0 && rooms[i-1][j].type != roomTypes.EMPTY){
+    				if(rooms[i-1][j].canGo.contains(Direction.DOWN) ){
+    					requires.add(Direction.UP);
+    					roomOptions.retainAll(up);
+    				}
+    			}
+    			
+    			
+    			//finally, pick one of the remaining options
+    			//at random and assign it
+    		}
+    	}
     }
 
-    private static boolean isPath(String[][] map, littleRoom[][] rooms) {
+	private static boolean isPath(String[][] map, littleRoom[][] rooms) {
         int x = map.length;
         int y = map[0].length;
         int p1Loc = -1;
@@ -135,7 +309,6 @@ public class MapMaker {
                     p2Loc = i*y + j;
                 if(map[i][j].contains("W"))
                     wumpusLoc = i*y + j;
-                
             }
         }
         
@@ -143,30 +316,179 @@ public class MapMaker {
         Boolean[][] haveVisited = new Boolean[x][y];
         
         toVisit.add(p1Loc);
-        toVisit.add(p2Loc);
-        toVisit.add(wumpusLoc);
         
         while(toVisit.size() > 0){
             Integer curRoom = toVisit.remove();
             Integer curX = curRoom / x;
             Integer curY = curRoom % x;
-            if(!haveVisited[curX][curY]){
-                switch (rooms[curX][curY].type){ //add appropriate things to queue
-                case CROSS:
-                    break;
-                case UP_RIGHT:
-                    break;
-                case UP_LEFT:
-                    break;
-                case UP_DOWN:
-                    break;
-                }
-                    
-                
+            littleRoom currentRoom = rooms[curX][curY];
+            if(!haveVisited[curX][curY] && 
+            		!currentRoom.isPit && !currentRoom.isBat){
+            	switch (currentRoom.type){ //add appropriate things to queue
+	                case CROSS:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY));
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY));
+                			haveVisited[curX-1][curY] = true;
+	                	}
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1));
+                			haveVisited[curX][curY + 1] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1));
+            				haveVisited[curX][curY - 1] = true;
+	                	}
+	                    break;
+	                case UP_RIGHT:
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY));
+	                		haveVisited[curX-1][curY] = true;
+	                	}
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1));
+	                		haveVisited[curX][curY + 1] = true;
+            			}
+            			break;
+	                case UP_LEFT:
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY));
+	                		haveVisited[curX-1][curY] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1));
+	                		haveVisited[curX][curY - 1] = true;
+	                	}
+	                    break;
+	                case UP_DOWN:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY));
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY));
+	                		haveVisited[curX-1][curY] = true;
+	                	}
+	                    break;
+	                case DOWN_RIGHT:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY));
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1)); 
+	                		haveVisited[curX][curY + 1] = true;
+	                	}
+	                	break;
+	                case DOWN_LEFT:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY)); 
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1));
+	                		haveVisited[curX][curY - 1] = true;
+	                	}
+	                	break;                
+	                case LEFT_RIGHT:
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1));
+	                		haveVisited[curX][curY + 1] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1)); 
+	                		haveVisited[curX][curY - 1] = true;
+	                		}
+	                	break;
+	                case T_RIGHT:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY)); 
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY)); 
+	                		haveVisited[curX-1][curY] = true;
+	                	}
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1));
+	                		haveVisited[curX][curY + 1] = true;
+	                	}
+	                    break;
+	                case T_LEFT:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY));
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY)); 
+	                		haveVisited[curX-1][curY] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1)); 
+	                		haveVisited[curX][curY - 1] = true;
+	                	}
+	                    break;
+	                case T_UP:
+	                	if(curX - 1 >= 0 && !haveVisited[curX-1][curY]){ //add up
+	                		toVisit.add((curX - 1) * y + (curY)); 
+	                		haveVisited[curX-1][curY] = true;
+	                		}
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1)); 
+	                		haveVisited[curX][curY + 1] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1)); 
+	                		haveVisited[curX][curY - 1] = true;
+	                	}
+	                    break;
+	                case T_DOWN:
+	                	if(curX + 1 < x && !haveVisited[curX+1][curY]){ //add down
+	                		toVisit.add((curX + 1) * y + (curY)); 
+	                		haveVisited[curX+1][curY] = true;
+	                	}
+	                	if(curY + 1 < y && !haveVisited[curX][curY+1]){ //add right
+	                		toVisit.add((curX) * y + (curY + 1));
+	                		haveVisited[curX][curY + 1] = true;
+	                	}
+	                	if(curY - 1 >= 0 && !haveVisited[curX][curY-1]){ //add left
+	                		toVisit.add((curX) * y + (curY - 1)); 
+	                		haveVisited[curX][curY - 1] = true;
+	                	}
+	                    break;
+	                case EMPTY:
+	                	System.out.println(
+	                			"Something went wrong, went to empty square");
+	                    return false;
+	                default:
+	                	System.out.println(
+	                			"Something went wrong, unaccoutned for state");
+	                	return false;
+                }    
             }
         }
-            
-        return false;
+        boolean isGood = true;
+        for(int i = 0; i < x; i++){
+        	for( int j = 0; j < y; j++){
+        		littleRoom currentRoom = rooms[i][j];
+        		if(currentRoom.type != roomTypes.EMPTY 
+        				&& !currentRoom.isBat && !currentRoom.isPit){
+        			if(!haveVisited[i][j]){
+        				isGood = false;
+        				System.out.println(
+        						"Did not visit room [" + x + "][" + y +"]");
+        			}
+        		}
+        				
+        	}
+        }
+        if(isGood)
+        	return true;
+        else
+        	return false;
     }
 
 }
