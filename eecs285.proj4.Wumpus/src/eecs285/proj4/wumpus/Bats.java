d51d4213b63d248169b47cf2e3f7b99ffb36b987
jessdev <jessdev@umich.edu>
2013-12-01 01:48:25 -0500

    add new trap type bats

---
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/Bats.java
@@ -0,0 +1,15 @@
+package eecs285.proj4.wumpus;
+
+public class Bats extends Trap{
+	
+	Bats(Room inRoom){
+		super(inRoom);
+	}
+
+	String callHint() {
+		return "You hear soft fluttering";
+	}
+	
+	
+
+}
