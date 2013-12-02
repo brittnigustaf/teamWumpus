0d8ff2227db34616d4dc1fad0e0a8e106c689b2a
brittnigustaf <bgustaf@umich.edu>
2013-12-02 06:37:25 -0500

    More Images to ImageList. Options GUI in GameWind

--- eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/GameWindow.java
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/GameWindow.java
@@ -1,8 +1,11 @@
 package eecs285.proj4.wumpus;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
 @SuppressWarnings("serial")
 public class GameWindow extends JFrame {
@@ -13,6 +16,8 @@ public class GameWindow extends JFrame {
 	Room curRoom = new Room();
   
 	Room roomMap[][];
+	
+	boolean fireMode;
 	
 	ImageIcon haveArrow = new ImageIcon();
     ImageIcon moveRight = new ImageIcon();
@@ -28,7 +33,7 @@ public class GameWindow extends JFrame {
     BorderLayout mainLayout = new BorderLayout();
     GridLayout mapLayout;
     FlowLayout informationLayout = new FlowLayout();
-    FlowLayout optionLayout = new FlowLayout();
+    GridLayout optionLayout = new GridLayout(3,1);
     FlowLayout infoLayout = new FlowLayout();
 
     
@@ -36,6 +41,21 @@ public class GameWindow extends JFrame {
     JPanel infoPane = new JPanel(infoLayout);
     JPanel gamePane;
     JPanel pane = new JPanel(mainLayout);
+    
+    //Player Variables
+    ArrayList<TitledBorder> playerBorders;
+    ArrayList<JLabel> playerScore;
+    
+    //Direction Panel members
+    JButton northButton;
+    JButton southButton;
+    JButton westButton;
+    JButton eastButton;
+    
+    TitledBorder dirPanTitle;
+    
+    //Game Mode Panel members
+    JButton changeMode;
     
     GameWindow(){
         //default 8x8, 1-player game
@@ -66,6 +86,8 @@ public class GameWindow extends JFrame {
         // inherit main frame
         Container con = this.getContentPane();
         
+        JFrame window = new JFrame("Hunt the Wumpus!");
+        
         //fill gamePane with roomIcons and find starting player position
         gamePane.setBounds(0, 0, 800, 600);
         gamePane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
@@ -79,22 +101,114 @@ public class GameWindow extends JFrame {
         optionPane.setPreferredSize(new Dimension(250, 600));
         optionPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
+        //Initialize Empty map!
+        ImageList images = new ImageList();
+        for (int i = 0; i < 8; i++)
+        {
+          for (int j = 0; j < 8; j++)
+          {
+            JLabel emptyMap = new JLabel(images.EMPTY);
+            gamePane.add(emptyMap);
+          } 
+        }
+        
+        //Build Player Score Info Panel
+        JPanel playerInfo = new JPanel(new GridLayout(2,1));
+        
+        //Initialize player (FOR DEBUGGING!)
+        players[0] = new Player();
+        
+        int i = 0;
+        playerScore = new ArrayList<JLabel>();
+        playerBorders = new ArrayList<TitledBorder>();
+        
+        //setup player Info
+        for(Player player : players){
+        
+          //Player Score Info Panel
+          String scoreMessage = "   " + player.name() + ":   " + Integer.toString(player.getScore());
+          JLabel scoreLabel = new JLabel(scoreMessage);
+          scoreLabel.setFont(new Font(scoreLabel.getName(), Font.PLAIN, 18));
+          playerScore.add(scoreLabel);
+          
+          playerInfo.add(playerScore.get(i));
+          
+          i++;
+        }
+        
+        //Player Score Border
+        TitledBorder scoreBorder = BorderFactory.createTitledBorder("Player Scores");
+        scoreBorder.setTitleJustification(TitledBorder.LEFT);
+        playerInfo.setBorder(scoreBorder);
+        
+        curPlayer = players[0];
+        
+        //Create info on top of info pane
+        JPanel topInfoPanel = new JPanel(new FlowLayout());
+        
+        //Player's turn Pane
+        JPanel turnMessage = new JPanel(new FlowLayout());
+        
+        JLabel playersTurnMessage = new JLabel("Current Players Turn: ");
+        
+        JLabel curPlayerName = new JLabel(curPlayer.name());
+        curPlayerName.setFont(new Font(curPlayerName.getName(), Font.PLAIN, 18));
+        
+        turnMessage.add(playersTurnMessage);
+        turnMessage.add(curPlayerName);
+        
+        //Sets the initial fire mode to false
+        fireMode = false;
+        
+        JLabel curGameMode = new JLabel("Current Mode: Move");
+        changeMode = new JButton("Change to Fire Mode");
+        
+        topInfoPanel.add(turnMessage);
+        topInfoPanel.add(new JLabel("                                                          "));
+        topInfoPanel.add(curGameMode);
+        topInfoPanel.add(changeMode);
+        
+        //Create Direction pane
+        JPanel dirPanel = new JPanel(new GridLayout(3,3));
+
+        northButton = new JButton(images.northArrow);
+        southButton = new JButton(images.southArrow);
+        westButton = new JButton(images.westArrow);
+        eastButton = new JButton(images.eastArrow);
+       
+        dirPanel.add(new JLabel());
+        dirPanel.add(northButton);
+        dirPanel.add(new JLabel());
+        dirPanel.add(westButton);
+        dirPanel.add(new JLabel());
+        dirPanel.add(eastButton);
+        dirPanel.add(new JLabel());
+        dirPanel.add(southButton);
+        dirPanel.add(new JLabel());
+
+        //Add direction panel titled border
+        dirPanTitle = BorderFactory.createTitledBorder("Move Directions");
+        dirPanTitle.setTitleJustification(TitledBorder.LEFT);
+        dirPanel.setBorder(dirPanTitle);
+        
+        //Add all of the option Panes
+        optionPane.add(topInfoPanel);
+        optionPane.add(dirPanel);
+        optionPane.add(playerInfo);
         
         //set panes
         pane.add(gamePane, BorderLayout.WEST);
         pane.add(infoPane, BorderLayout.SOUTH);
         pane.add(optionPane, BorderLayout.EAST);
         
-        //setup player Info
-        for(Player player : players){
-            
-        }
         
-        curPlayer = players[0];
+        // add the panel to frame      
+        window.add(pane);
+        window.pack();
+        window.setVisible(true);
         
-        // add the panel to frame
-        con.add(pane);
-        setVisible(true);
+        //con.add(pane);
+        //setVisible(true);
         
     }
     
@@ -103,6 +217,7 @@ public class GameWindow extends JFrame {
     }
     
     void fire(Directions dir){
+      //dirPanTitle.setTitle("Fire Directions");
         
     }
     
--- eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/ImageList.java
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/ImageList.java
@@ -1,40 +1,273 @@
 package eecs285.proj4.wumpus;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import javax.swing.ImageIcon;
-
+import javax.swing.JLabel;
+ 
 public class ImageList {
-	//member variables
-	
-	static ImageIcon CROSS; 
-	static ImageIcon UP_RIGHT;
-	static ImageIcon UP_LEFT; 
-	static ImageIcon UP_DOWN; 
-	static ImageIcon DOWN_RIGHT;
-	static ImageIcon DOWN_LEFT;
-    static ImageIcon LEFT_RIGHT;
-    static ImageIcon T_RIGHT;
-    static ImageIcon T_LEFT;
-    static ImageIcon T_UP;
-    static ImageIcon T_DOWN;
-    static ImageIcon EMPTY;
-
+  //Tile Members
+  static ImageIcon CROSS;
+  static ImageIcon UP_RIGHT;
+  static ImageIcon UP_LEFT;
+  static ImageIcon UP_DOWN; 
+  static ImageIcon DOWN_RIGHT;
+  static ImageIcon DOWN_LEFT;
+  static ImageIcon LEFT_RIGHT;
+  static ImageIcon T_RIGHT;
+  static ImageIcon T_LEFT;
+  static ImageIcon T_UP;
+  static ImageIcon T_DOWN;
+  static ImageIcon EMPTY;
+  
+  //Trap Members
+  static ImageIcon BAT;
+  static ImageIcon WUMPUS;
+  static ImageIcon PIT;
+  
+  //GUI Image Members
+  static ImageIcon northArrow;
+  static ImageIcon eastArrow;
+  static ImageIcon southArrow;
+  static ImageIcon westArrow;
+  
+  ImageList(){
+    //EFF: imports all the images
     
-    ImageList(){
-    	//EFF: imports all the images
-    	
-    	CROSS = new ImageIcon(); 
-        UP_RIGHT = new ImageIcon();
-        UP_LEFT = new ImageIcon();
-        UP_DOWN = new ImageIcon();
-        DOWN_RIGHT = new ImageIcon();
-        DOWN_LEFT = new ImageIcon();
-        LEFT_RIGHT = new ImageIcon();
-        T_RIGHT = new ImageIcon();
-        T_LEFT = new ImageIcon();
-        T_UP = new ImageIcon();
-        T_DOWN = new ImageIcon();
-        EMPTY = new ImageIcon();
-        
+    //Tile Members
+    BufferedImage CROSS_img = null;
+    BufferedImage UP_RIGHT_img = null;
+    BufferedImage UP_LEFT_img = null;
+    BufferedImage UP_DOWN_img = null;
+    BufferedImage DOWN_RIGHT_img = null;
+    BufferedImage DOWN_LEFT_img = null;
+    BufferedImage LEFT_RIGHT_img = null;
+    BufferedImage T_RIGHT_img = null;
+    BufferedImage T_LEFT_img = null;
+    BufferedImage T_UP_img = null;
+    BufferedImage T_DOWN_img = null;
+    BufferedImage EMPTY_img = null;
+    
+    try {
+      CROSS_img = ImageIO.read(new File("images/cross_tile.png"));
+      UP_RIGHT_img = ImageIO.read(new File("images/en_tile.png"));
+      UP_LEFT_img = ImageIO.read(new File("images/wn_tile.png"));
+      UP_DOWN_img = ImageIO.read(new File("images/ns_tile.png"));
+      DOWN_RIGHT_img = ImageIO.read(new File("images/es_tile.png"));
+      DOWN_LEFT_img = ImageIO.read(new File("images/ws_tile.png"));
+      LEFT_RIGHT_img = ImageIO.read(new File("images/ew_tile.png"));
+      T_RIGHT_img = ImageIO.read(new File("images/nes_tile.png"));
+      T_LEFT_img = ImageIO.read(new File("images/nsw_tile.png"));
+      T_UP_img = ImageIO.read(new File("images/new_tile.png"));
+      T_DOWN_img = ImageIO.read(new File("images/esw_tile.png"));
+      EMPTY_img = ImageIO.read(new File("images/empty_tile.png"));
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
     }
-
+    
+    BufferedImage CROSS_resized = resize(CROSS_img, 100, 75);
+    BufferedImage UP_RIGHT_resized = resize(UP_RIGHT_img, 100, 75);
+    BufferedImage UP_LEFT_resized = resize(UP_LEFT_img, 100, 75);
+    BufferedImage UP_DOWN_resized = resize(UP_DOWN_img, 100, 75);
+    BufferedImage DOWN_RIGHT_resized = resize(DOWN_RIGHT_img, 100, 75);
+    BufferedImage DOWN_LEFT_resized = resize(DOWN_LEFT_img, 100, 75);
+    BufferedImage LEFT_RIGHT_resized = resize(LEFT_RIGHT_img, 100, 75);
+    BufferedImage T_RIGHT_resized = resize(T_RIGHT_img, 100, 75);
+    BufferedImage T_LEFT_resized = resize(T_LEFT_img, 100, 75);
+    BufferedImage T_UP_resized = resize(T_UP_img, 100, 75);
+    BufferedImage T_DOWN_resized = resize(T_DOWN_img, 100, 75);
+    BufferedImage EMPTY_resized = resize(EMPTY_img, 100, 75);
+    
+    CROSS = new ImageIcon(CROSS_resized); 
+    UP_RIGHT = new ImageIcon(UP_RIGHT_resized);
+    UP_LEFT = new ImageIcon(UP_LEFT_resized);
+    UP_DOWN = new ImageIcon(UP_DOWN_resized);
+    DOWN_RIGHT = new ImageIcon(DOWN_RIGHT_resized);
+    DOWN_LEFT = new ImageIcon(DOWN_LEFT_resized);
+    LEFT_RIGHT = new ImageIcon(LEFT_RIGHT_resized);
+    T_RIGHT = new ImageIcon(T_RIGHT_resized);
+    T_LEFT = new ImageIcon(T_LEFT_resized);
+    T_UP = new ImageIcon(T_UP_resized);
+    T_DOWN = new ImageIcon(T_DOWN_resized);
+    EMPTY = new ImageIcon(EMPTY_resized);
+    
+    //Trap Members
+    BufferedImage BAT_img = null;
+    BufferedImage WUMPUS_img = null;
+    BufferedImage PIT_img = null;
+    
+    try {
+      BAT_img = ImageIO.read(new File("images/bat_tile.png"));
+      WUMPUS_img = ImageIO.read(new File("images/wumpus.jpg"));
+      PIT_img = ImageIO.read(new File("images/pit_tile.png"));
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    BufferedImage BAT_resized = resize(BAT_img, 100, 75);
+    BufferedImage WUMPUS_resized = resize(WUMPUS_img, 100, 75);
+    BufferedImage PIT_resized = resize(PIT_img, 100, 75);
+    
+    BAT = new ImageIcon(BAT_resized); 
+    WUMPUS = new ImageIcon(WUMPUS_resized);
+    PIT = new ImageIcon(PIT_resized);
+    
+    //GUI Members
+    BufferedImage northArrow_img = null;
+    BufferedImage eastArrow_img = null;
+    BufferedImage southArrow_img = null;
+    BufferedImage westArrow_img = null;
+    
+    try {
+      northArrow_img = ImageIO.read(new File("images/arrows/north_arrow.png"));
+      eastArrow_img = ImageIO.read(new File("images/arrows/east_arrow.png"));
+      southArrow_img = ImageIO.read(new File("images/arrows/south_arrow.png"));
+      westArrow_img = ImageIO.read(new File("images/arrows/west_arrow.png"));
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    BufferedImage northArrow_resized = resize(northArrow_img, 40, 40);
+    BufferedImage eastArrow_resized = resize(eastArrow_img, 40, 40);
+    BufferedImage southArrow_resized = resize(southArrow_img, 40, 40);
+    BufferedImage westArrow_resized = resize(westArrow_img, 40, 40);
+    
+    northArrow = new ImageIcon(northArrow_resized); 
+    eastArrow = new ImageIcon(eastArrow_resized);
+    southArrow = new ImageIcon(southArrow_resized);
+    westArrow = new ImageIcon(westArrow_resized);
+       
+   }
+  
+  //Resizes image based on given width and height
+  ImageList(int width, int height){
+    //EFF: imports all the images
+    
+    BufferedImage CROSS_img = null;
+    BufferedImage UP_RIGHT_img = null;
+    BufferedImage UP_LEFT_img = null;
+    BufferedImage UP_DOWN_img = null;
+    BufferedImage DOWN_RIGHT_img = null;
+    BufferedImage DOWN_LEFT_img = null;
+    BufferedImage LEFT_RIGHT_img = null;
+    BufferedImage T_RIGHT_img = null;
+    BufferedImage T_LEFT_img = null;
+    BufferedImage T_UP_img = null;
+    BufferedImage T_DOWN_img = null;
+    BufferedImage EMPTY_img = null;
+    
+    try {
+      CROSS_img = ImageIO.read(new File("images/cross_tile.png"));
+      UP_RIGHT_img = ImageIO.read(new File("images/en_tile.png"));
+      UP_LEFT_img = ImageIO.read(new File("images/wn_tile.png"));
+      UP_DOWN_img = ImageIO.read(new File("images/ns_tile.png"));
+      DOWN_RIGHT_img = ImageIO.read(new File("images/es_tile.png"));
+      DOWN_LEFT_img = ImageIO.read(new File("images/ws_tile.png"));
+      LEFT_RIGHT_img = ImageIO.read(new File("images/ew_tile.png"));
+      T_RIGHT_img = ImageIO.read(new File("images/nes_tile.png"));
+      T_LEFT_img = ImageIO.read(new File("images/nsw_tile.png"));
+      T_UP_img = ImageIO.read(new File("images/new_tile.png"));
+      T_DOWN_img = ImageIO.read(new File("images/esw_tile.png"));
+      EMPTY_img = ImageIO.read(new File("images/empty_tile.png"));
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    BufferedImage CROSS_resized = resize(CROSS_img, width, height);
+    BufferedImage UP_RIGHT_resized = resize(UP_RIGHT_img, width, height);
+    BufferedImage UP_LEFT_resized = resize(UP_LEFT_img, width, height);
+    BufferedImage UP_DOWN_resized = resize(UP_DOWN_img, width, height);
+    BufferedImage DOWN_RIGHT_resized = resize(DOWN_RIGHT_img, width, height);
+    BufferedImage DOWN_LEFT_resized = resize(DOWN_LEFT_img, width, height);
+    BufferedImage LEFT_RIGHT_resized = resize(LEFT_RIGHT_img, width, height);
+    BufferedImage T_RIGHT_resized = resize(T_RIGHT_img, width, height);
+    BufferedImage T_LEFT_resized = resize(T_LEFT_img, width, height);
+    BufferedImage T_UP_resized = resize(T_UP_img, width, height);
+    BufferedImage T_DOWN_resized = resize(T_DOWN_img, width, height);
+    BufferedImage EMPTY_resized = resize(EMPTY_img, width, height);
+    
+    CROSS = new ImageIcon(CROSS_resized); 
+    UP_RIGHT = new ImageIcon(UP_RIGHT_resized);
+    UP_LEFT = new ImageIcon(UP_LEFT_resized);
+    UP_DOWN = new ImageIcon(UP_DOWN_resized);
+    DOWN_RIGHT = new ImageIcon(DOWN_RIGHT_resized);
+    DOWN_LEFT = new ImageIcon(DOWN_LEFT_resized);
+    LEFT_RIGHT = new ImageIcon(LEFT_RIGHT_resized);
+    T_RIGHT = new ImageIcon(T_RIGHT_resized);
+    T_LEFT = new ImageIcon(T_LEFT_resized);
+    T_UP = new ImageIcon(T_UP_resized);
+    T_DOWN = new ImageIcon(T_DOWN_resized);
+    EMPTY = new ImageIcon(EMPTY_resized);
+    
+  //Trap Members
+    BufferedImage BAT_img = null;
+    BufferedImage WUMPUS_img = null;
+    BufferedImage PIT_img = null;
+    
+    try {
+      BAT_img = ImageIO.read(new File("images/bat_tile.png"));
+      WUMPUS_img = ImageIO.read(new File("images/wumpus.jpg"));
+      PIT_img = ImageIO.read(new File("images/pit_tile.png"));
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    BufferedImage BAT_resized = resize(BAT_img, width, height);
+    BufferedImage WUMPUS_resized = resize(WUMPUS_img, width, height);
+    BufferedImage PIT_resized = resize(PIT_img, width, height);
+    
+    BAT = new ImageIcon(BAT_resized); 
+    WUMPUS = new ImageIcon(WUMPUS_resized);
+    PIT = new ImageIcon(PIT_resized);
+    
+    //GUI Members
+    BufferedImage northArrow_img = null;
+    BufferedImage eastArrow_img = null;
+    BufferedImage southArrow_img = null;
+    BufferedImage westArrow_img = null;
+    
+    try {
+      northArrow_img = ImageIO.read(new File("images/arrows/north_arrow.png"));
+      eastArrow_img = ImageIO.read(new File("images/arrows/east_arrow.png"));
+      southArrow_img = ImageIO.read(new File("images/arrows/south_arrow.png"));
+      westArrow_img = ImageIO.read(new File("images/arrows/west_arrow.png"));
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    BufferedImage northArrow_resized = resize(northArrow_img, 40, 40);
+    BufferedImage eastArrow_resized = resize(eastArrow_img, 40, 40);
+    BufferedImage southArrow_resized = resize(southArrow_img, 40, 40);
+    BufferedImage westArrow_resized = resize(westArrow_img, 40, 40);
+    
+    northArrow = new ImageIcon(northArrow_resized); 
+    eastArrow = new ImageIcon(eastArrow_resized);
+    southArrow = new ImageIcon(southArrow_resized);
+    westArrow = new ImageIcon(westArrow_resized);
+      
+  }
+  
+  
+  //Resizes an image
+  public static BufferedImage resize(BufferedImage image, int width, int height) {
+    BufferedImage buffedImage = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
+    Graphics2D g2d = (Graphics2D) buffedImage.createGraphics();
+    g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+    g2d.drawImage(image, 0, 0, width, height, null);
+    g2d.dispose();
+    return buffedImage;
+  }
 }
