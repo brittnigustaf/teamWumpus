492275c25188c04a24ebf590af1676979324abb4
jessdev <jessdev@umich.edu>
2013-12-01 21:44:04 -0500

    added a special place for wumpus in the room.

--- eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/Dwarf.java
+++ eecs285.proj4.Wumpus/src/eecs285/proj4/wumpus/Dwarf.java
@@ -151,19 +151,19 @@ public class Dwarf {
 			}
 			
 			if(plans[i].equals(codex.PITFALL)){
-				dungeon[cor[0]][cor[1]].add(new Pitfall(dungeon[cor[0]][cor[1]]));
+				dungeon[cor[0]][cor[1]].addTrap(new Pitfall(dungeon[cor[0]][cor[1]]));
 			}
 			  
 			if(plans[i].equals(codex.BATS)){
-				dungeon[cor[0]][cor[1]].add(new Bats(dungeon[cor[0]][cor[1]]));
+				dungeon[cor[0]][cor[1]].addTrap(new Bats(dungeon[cor[0]][cor[1]]));
 			}
 			  
 			if(plans[i].equals(codex.WUMPUS)){
-				dungeon[cor[0]][cor[1]].add(new Wumpus(dungeon[cor[0]][cor[1]]));
+				dungeon[cor[0]][cor[1]].addWumpus(new Wumpus(dungeon[cor[0]][cor[1]]));
 			}
 			
 			if(plans[i].equals(codex.GOLD)){
-				dungeon[cor[0]][cor[1]].add(new Gold(dungeon[cor[0]][cor[1]]));
+				dungeon[cor[0]][cor[1]].addTrap(new Gold(dungeon[cor[0]][cor[1]]));
 			}
 			  
 		  }
