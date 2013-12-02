a54d8d8b08ebb4e56e589f38d3083f8bf2a01255
Julian <Julian@67.194.28.88>
2013-12-02 14:38:26 -0500

    Working Help page

--- eecs285.proj4.Wumpus/eecs285/proj4/wumpus/HelpWindow.java
+++ eecs285.proj4.Wumpus/eecs285/proj4/wumpus/HelpWindow.java
@@ -16,13 +16,8 @@ public class HelpWindow extends JFrame {
     JPanel helpPane = new JPanel();
     JPanel pane = new JPanel(mainLayout);
     JLabel helpText = new JLabel();
-    /*JLabel instructiontext1 = new JLabel();
-    JLabel instructiontext2 = new JLabel();
-    JLabel instructiontext3 = new JLabel();
-    JPanel instructionPane1 = new JPanel();
-    JPanel instructionPane2 = new JPanel();
-    JPanel instructionPane3 = new JPanel();*/
-    JTextArea instructions = new JTextArea(6,30);
+
+    JTextArea instructions = new JTextArea(6,20);
     
     HelpWindow(){
         super("Instructions");
@@ -42,12 +37,7 @@ public class HelpWindow extends JFrame {
         
         helpText.setText("How to Play!");
         helpPane.add(helpText);
-        /*instructiontext1.setText("Gather gold, avoid the traps, and kill the dreaded Wumpus!");
-        instructiontext2.setText("Click on the arrows to go in the direction you want to go!");
-        instructiontext3.setText("Read the Clues!");
-        instructionPane1.add(instructiontext1);
-        instructionPane2.add(instructiontext2);
-        instructionPane3.add(instructiontext3);*/
+
         instructions.setEditable(false);
         instructions.append("Gather gold, avoid the traps, and kill the dreaded Wumpus!\n"
         		+ "Click on the arrows to go in the direction you want to go!\n\n" 
@@ -66,9 +56,7 @@ public class HelpWindow extends JFrame {
         pane.add(helpPane, BorderLayout.NORTH);
         pane.add(btnPane, BorderLayout.SOUTH);
         pane.add(instructions, BorderLayout.CENTER);
-        /*pane.add(instructionPane1);
-        pane.add(instructionPane2, BorderLayout.CENTER);
-        pane.add(instructionPane3);//, BorderLayout.CENTER);*/
+
     }
 
 }
